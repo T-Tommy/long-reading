4c26396074c1c7f22f70cd53b05c76eb9925d4e9
Tommy Tran <tranttommy95@gmail.com>
2019-04-14 23:39:47 -0700

    What's needed to "route" requests?

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const http = require('http');
+const url = require('url');
 
-function start() {
+function start(route) {
 
   function onRequest(request, response) {
-    console.log('Request received.');
+    const pathname = url.parse(request.url).pathname;
+    console.log(`Request for ${pathname} received.`);
+
+    route(pathname);
+
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World');
     response.end();
   }
-  
-  http.createServer(onRequest).listen(4242); 
+
+  http.createServer(onRequest).listen(4242);
   console.log('Server has started.');
 }
 
