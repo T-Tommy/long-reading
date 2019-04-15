5fdafe0d26b37ab056d4c7894a5f1347f48f3023
Tommy Tran <tranttommy95@gmail.com>
2019-04-15 00:31:27 -0700

    Handling POST requests

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const url = require('url');
 function start(route, handle) {
 
   function onRequest(request, response) {
+    let postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
+    
+    request.setEncoding('utf8');
+    
+    request.addListener('data', chunk => {
+      postData += chunk;
+      console.log(`Received POST data chunk "${chunk}".`);
+    });
 
-    route(handle, pathname, response);
+    request.addListener('end', () => {
+      route(handle, pathname, response, postData);
+    });
   }
 
   http.createServer(onRequest).listen(4242);
